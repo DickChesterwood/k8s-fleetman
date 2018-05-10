0ef32b6546bb6ee3eefbed7e29a0fccfe622530c
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-10 18:54:18 +0100

    untangling

--- k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
+++ k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
@@ -1,4 +1,4 @@
-package com.virtualpairprogrammers;
+package com.virtualpairprogrammers.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
