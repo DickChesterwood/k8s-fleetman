8c77570fbe76e3dce407f9e005e4572a43bf98be
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-10 14:55:56 +0100

    unit tests passing again for position tracker

--- k8s-fleetman-position-tracker/src/main/java/com/virtualpairprogrammers/data/DataBasicInMemoryImpl.java
+++ k8s-fleetman-position-tracker/src/main/java/com/virtualpairprogrammers/data/DataBasicInMemoryImpl.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -50,7 +49,7 @@ public class DataBasicInMemoryImpl implements Data
 			positions = new TreeSet<>();
 			positionDatabase.put(vehicleName, positions);
 		}
-		BigDecimal speed = calculateSpeedInMph(vehicleName);
+		BigDecimal speed = calculateSpeedInMph(vehicleName, data);
 		VehiclePosition vehicleWithSpeed = new VehicleBuilder().withVehiclePostion(data).withSpeed(speed).build();
 		positions.add(vehicleWithSpeed);
 	}
@@ -63,18 +62,13 @@ public class DataBasicInMemoryImpl implements Data
 		return reportsForThisVehicle.first();
 	}
 	
-	private BigDecimal calculateSpeedInMph(String vehicleName)
+	private BigDecimal calculateSpeedInMph(String vehicleName, VehiclePosition newPosition)
 	{	
-		// we know we have exactly two positions in the queue....
-		Collection<VehiclePosition> queue = positionDatabase.get(vehicleName);
-		Iterator<VehiclePosition> it = queue.iterator();
-		VehiclePosition posA;
-		VehiclePosition posB;		
-		synchronized(this)
-		{
-			posA = it.next();
-			posB = it.next();
-		}
+		TreeSet<VehiclePosition> positions = positionDatabase.get(vehicleName);
+		if (positions.isEmpty()) return null;
+		
+		VehiclePosition posB = newPosition;
+		VehiclePosition posA = positions.first(); // confusing - this is actually the last report recorded
 		
 		long timeAinMillis = posA.getTimestamp().getTime();
 		long timeBinMillis = posB.getTimestamp().getTime();
@@ -88,7 +82,6 @@ public class DataBasicInMemoryImpl implements Data
 	
 		double distance = geoCalc.calculateGeodeticCurve(Ellipsoid.WGS84, pointA, pointB).getEllipsoidalDistance(); // Distance between Point A and Point B
 		BigDecimal distanceInMetres = new BigDecimal (""+ distance);
-		
 		
 		BigDecimal speedInMps = distanceInMetres.divide(timeInSeconds, RoundingMode.HALF_UP);
 		BigDecimal milesPerHour = speedInMps.multiply(MPS_TO_MPH_FACTOR);
@@ -110,6 +103,7 @@ public class DataBasicInMemoryImpl implements Data
 		TreeSet<VehiclePosition> vehicleReports = this.positionDatabase.get(name);
 		if (vehicleReports == null) throw new VehicleNotFoundException();
 		
+		// TODO replace with headset or tailset!
 		for (VehiclePosition next: vehicleReports)
 		{
 			Date timeStampOfHistoricReport = next.getTimestamp();
