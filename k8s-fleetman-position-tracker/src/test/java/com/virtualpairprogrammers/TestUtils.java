e91fbf64508386ac4ed5e4d5ffe88df85e10028c
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-13 13:21:52 +0100

    case #9

--- k8s-fleetman-position-tracker/src/test/java/com/virtualpairprogrammers/TestUtils.java
+++ k8s-fleetman-position-tracker/src/test/java/com/virtualpairprogrammers/TestUtils.java
@@ -4,11 +4,12 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 class TestUtils {
 	public static Date getDateFrom(String timestamp)
 	{
-		DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+		DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
 		try {
 			return format.parse(timestamp);
 		} 
