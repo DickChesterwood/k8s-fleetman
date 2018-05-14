eab75c8cce787433760f72ef8e6407f89dbcfee0
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-14 15:45:14 +0100

    case #17 position-tracker

--- k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/CloudConfiguration.java
+++ k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/CloudConfiguration.java
@@ -3,7 +3,6 @@ package com.virtualpairprogrammers.api;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCircuitBreaker
