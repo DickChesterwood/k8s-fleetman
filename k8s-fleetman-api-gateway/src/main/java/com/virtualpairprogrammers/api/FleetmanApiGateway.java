15661ca25238a7aabf88ac9b502afa7e5bd61bc4
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-14 15:30:04 +0100

    case #17 api-gateway

--- k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
+++ k8s-fleetman-api-gateway/src/main/java/com/virtualpairprogrammers/api/FleetmanApiGateway.java
@@ -1,7 +1,5 @@
 package com.virtualpairprogrammers.api;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
