993e0391a21d94ffb338e9e623e15dd5a7e0bbda
Dick Chesterwood <richard@inceptiontraining.co.uk>
2018-05-22 14:59:57 +0100

    case #18

--- k8s-fleetman-webapp-angular/src/app/app.module.ts
+++ k8s-fleetman-webapp-angular/src/app/app.module.ts
@@ -20,21 +20,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { DOCUMENT } from '@angular/platform-browser';
 
 const stompConfig: StompConfig = {
-     // Which server? TODO.
-     // url: "ws://" + window.location.hostname + ":30030/updates",
-     url: "ws://" + window.location.hostname + "/api/updates",
+     url: "ws://" + window.location.hostname + ":" + window.location.port + "/api/updates",
      headers: {
      },
-     // How often to heartbeat?
-     // Interval in milliseconds, set to 0 to disable
      heartbeat_in: 0, // Typical value 0 - disabled
      heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
-     // Wait in milliseconds before attempting auto reconnect
-     // Set to 0 to disable
-     // Typical value 5000 (5 seconds)
      reconnect_delay: 5000,
-
-     // Will log diagnostics on console
      debug: true
 };
 
